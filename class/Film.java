66010558735bca12cc8216b1010bca78a801de96
danieleberibe <48356575+danieleberibe@users.noreply.github.com>
2022-03-10 17:50:40 +0100

    class approssimate

--- class/Film.java
+++ class/Film.java
@@ -1,8 +1,26 @@
 public class Film {
-   public String name;
-   public String genere;
-   public String descrizione;
-   public int durata;
-   
-    
+   private String name;
+   private String genere;
+   private String descrizione;
+   private int durata;
+
+   public Film (String nome, String gen, String desc, int dur) {
+       name = nome;
+       genere = gen;
+       descrizione = desc;
+       durata = dur;
+   }
+
+   public String getName(){
+    return name;
+    }
+    public String getGenere(){
+        return genere;
+    }
+    public String getDescrizione(){
+        return descrizione;
+    }
+    public int getDurata(){
+        return durata;
+    }
 }
