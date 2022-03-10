e297597cc333eeee6f5ed8041b60a35e6d717cdc
gripan2 <mario.giannantonio997@gmail.com>
2022-03-10 23:16:55 +0100

    finito classi utente e prenotazione

---
+++ class/prenotazione.java
@@ -0,0 +1,36 @@
+public class prenotazione {
+    private int idPrenotazione;
+    private int prezzoPrenotazione;
+
+    public prenotazione (int idPrenotazione2, int prezzoPrenotazione2 ) { 
+
+      idPrenotazione2 = idPrenotazione;
+      prezzoPrenotazione2 = prezzoPrenotazione;
+    }
+
+    public int getIdPrenotazione() {
+        return idPrenotazione;
+    }
+    
+    public void setIdPrenotazione(int idPrenotazione2) {
+        idPrenotazione2 = idPrenotazione;
+    }
+
+    public int getPrezzoPrenotazione() {
+        return prezzoPrenotazione;
+    }
+    
+    public void setPrezzoPrenotazione(int prezzoPrenotazione2) {
+        prezzoPrenotazione2 = prezzoPrenotazione;
+    }
+
+
+ public calcoloDisponibilita(){
+    
+ }
+
+ public calcoloPrezzo(){
+
+ }
+
+ }
--- class/user.java
+++ class/user.java
@@ -1,8 +1,67 @@
 public class user  {
-    public String name ;
-    public String cognome;
-    public String email;
+    private String nome ;
+    private String cognome;
+    private String email;
     private String passwordUtente;
-    public String username;
-    public boolean registrato;
+    private String username;
+    private boolean signed;
+
+
+public user (String nome2, String cognome2, String email2, String passwordUtente2, String username2, boolean signed2 ) { 
+
+    nome2 = nome;
+    cognome2 = cognome;
+    email2 = email;
+    passwordUtente2 = passwordUtente;
+    username2 = username;
+
+}
+
+public String getNome() {
+	return nome;
+}
+
+public void setNome(String nome2) {
+	nome2 = nome;
+}
+
+public String getCognome() {
+	return cognome;  
+}
+
+public void setCognome(String cognome2) {
+	cognome2 = cognome;
+}
+public String getEmail() {
+	return nome;   
+}
+
+public void setEmail(String email2) {
+	email2 = email;
+}
+
+public String getPasswordutente() {
+	return passwordUtente;   
+}
+
+public void setPasswordutente(String passwordUtente2) {
+	passwordUtente2 = passwordUtente;
+}
+
+public String getUsername() {
+	return username;   
+}
+
+public void setUsername(String username2) {
+	username2 = username;
+}
+
+public boolean getSigned() {
+	return signed;   
+}
+
+public void setSigned(boolean signed2) {
+	signed2 = signed;
+}
+
 }
