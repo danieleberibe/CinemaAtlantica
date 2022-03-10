e5978d3c5e4dc78ca503ab873bd6a440596db3d4
danieleberibe <48356575+danieleberibe@users.noreply.github.com>
2022-03-10 16:57:05 +0100

    add class film

---
+++ class/Film.java
@@ -0,0 +1,8 @@
+public class Film {
+   public String name;
+   public String genere;
+   public String descrizione;
+   public int durata;
+   
+    
+}
--- class/prova.java
+++
@@ -1,5 +0,0 @@
-public static void name() {
-    //lol
-    //bella pe te 
-    //prova
-}
