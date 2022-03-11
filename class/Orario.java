73f5c896f96ec960d618d6c8429e047897e37084
gripan2 <mario.giannantonio997@gmail.com>
2022-03-11 11:57:27 +0100

    finita classe orario

---
+++ class/Orario.java
@@ -0,0 +1,36 @@
+public class Orario {
+    private int orario;
+    private int data;
+}
+
+public Orario (int orario2, int data2 ) { 
+
+    orario2 = orario;
+    data2 = data;
+
+}
+
+public int getOrario() {
+    return orario;
+}
+
+public void setOrario(int orario2) {
+    orario2 = orario;
+}
+
+public int getData(){
+    return data;
+}
+
+public void setData(int data2) {
+    data2 = data;
+}
+
+public calcoloDisponibilita() {
+    
+}
+
+public calcoloPrezzo() {
+
+}
+}
